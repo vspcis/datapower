88dc45e40b1d5aa2e11861b933a450af2fcb9fee
Victor Scaletchi <victor.scaletchi@ehealthontario.or.ca>
2018-01-09 16:56:48 -0500

    requests

--- datapower.py
+++ datapower.py
@@ -1,47 +1,22 @@
 """datapower"""
-import urllib2
-import ssl
 import json
-from settings import MGM_URI, USER, PASSWORD, TOP_LEVEL_URL
+import requests
+from settings import USER, PASSWORD, MGMT_URI, DOMAINS_URI, ALIASES_URI
 
 def main():
     """main"""
-    # get_domains()
-    # get_aliases()
-    get_config()
+    print get_config(MGMT_URI)
+    aliases = get_config(ALIASES_URI)['HostAlias']
+    for alias in aliases:
+        print alias['name'] + ': ' + alias['IPAddress']
+    domains = get_config(DOMAINS_URI)['domain']
+    for domain in domains:
+        print domain['name']
 
-def get_domains():
-    """read_config_from_file"""
 
-    with open('json/mgmt_domains_config.json') as json_data:
-        domains = json.load(json_data)['domain']
-        for domain in domains:
-            print domain["name"]
-
-def get_aliases():
-    """get_aliases"""
-
-    with open('json/mgmt_config_default_HostAlias.json') as json_data:
-        domains = json.load(json_data)['HostAlias']
-        for domain in domains:
-            print domain["name"] + ': ' + domain['IPAddress']
-
-def get_config():
-    """get_config - not working from desktop (basic auth / proxy"""
-    print MGM_URI
-    print USER
-    print PASSWORD
-
-    # datapower certificate cannot be validated
-    ssl._create_default_https_context = ssl._create_unverified_context # pylint: disable=W0212
-    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    password_mgr.add_password(None, TOP_LEVEL_URL, USER, PASSWORD)
-    password_handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-    proxy_handler = urllib2.ProxyHandler({})
-
-    opener = urllib2.build_opener(password_handler, proxy_handler)
-    response = opener.open(MGM_URI)
-    print response
+def get_config(url):
+    """get_config"""
+    return json.loads(requests.get(url, auth=(USER, PASSWORD), verify=False).content)
 
 if __name__ == '__main__':
     main()
--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 """settings"""
 MGMT_IP = '10.69.0.10'
 MGMT_PORT = '5554'
-MGM_URI = 'https://' + MGMT_IP + ':' + MGMT_PORT + '/mgmt/'
-TOP_LEVEL_URL = 'https://' + MGMT_IP
+MGMT_URI = 'https://' + MGMT_IP + ':' + MGMT_PORT + '/mgmt/'
+DOMAINS_URI = MGMT_URI + 'domains/config/'
+ALIASES_URI = MGMT_URI + 'config/default/HostAlias'
 
 USER = 'readonly'
 PASSWORD = '1234pcis'
